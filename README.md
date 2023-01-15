d11049c98e4b07b9bcab9e9f7ebe68c0d272f942
rob.tisdale <rtisdale.bt@gmail.com>
2023-01-15 10:44:25 -0600

    Homework 1: Complete

--- README.md
+++ README.md
@@ -1,19 +1,47 @@
 # MakeChangeProject
 
-############################################################
 # Description
 
-***give breakdown of project***
+The overall purpose of this program is to provide exact change of two numbers(price and tendered amount) using dollar and cent denominations <= 20 dollars (20, 10, 5, 1, qtr, dime, nickel, penny).
+
+# Instructions:
+Follow the prompts on the screen.
+1. Enter the value of your item
+2. Enter the amount to be tendered.
+3. See below for expected outcomes
+
+EXPECTED OUTCOMES:
+1. Tendered > Amount: Change message with denomination breakdown. 
+2. Tendered < Amount: Error message.
+3. Tendered + Amount: Perfect Change Message.
 
 
-############################################################
+# Code breakdown:
+Line
+8 Declaring scanner 
+12-20 Declaring variables for price and tender amount with prompts and scan.nextDouble to assign.
+24-29 Reassining original values to individual whole numbers and declaring and assigning new variables for methods.
+33-37 If statement for Expected outcomes above.
+40-55 Change statement + calling dollar and change method to execute. (If statement added as more of an aesthetic touch. Doesnt affect calculation)
+
+60-102 Method for breaking down dollars.
+Broken down by individual denominations. Each given its respective variable and calling in "dollars" for its calculations. 
+
+105-148 Method for breaking down cents.
+Same concept as dollars except bulling in "cents"
+
+
 # Technologies used
 
 1. Java
 
 2. Eclipse
 
-############################################################
 # Lessons Learned
+1. GET OUT OF MY HEAD. I spent way too much trying to use nested if statements (first commit) which worked at first but as it got bigger is was extremely hard to keep track. 
+2. Took some trial and error and an assist from the TAs but separating the dollars and cents and turning them to whole numbers first helps a lot.
+3. If "Hint: Mod Operator" is given, I shouldnt waste time with && / || if statements. Its provided for a reason. I wasted friday night and saturday morning. (I guess not wasted if I learned something).
+4. Wait to add all the bells and whistles AFTER is it 100% functional. I got carried away worrying about things that had nothing to do with the primary problem. 
+
 
 
--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -7,44 +7,53 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-///////***Initial variable and prompts***/////////////////////////////////////////////////////////////////
+//Initial variable and prompts
 
 		double price;
 		double amountTendered;
-		
-		System.out.println();
-		System.out.print("Please enter the price of the item: ");
-		price = scan.nextDouble();
 
-		System.out.print("Please enter the amount of money tendered: ");
-		amountTendered = scan.nextDouble();
+			System.out.print("Please enter the price of the item: ");
+			price = scan.nextDouble();
+			scan.nextLine();
 
-///////***Response for Exact change or too little***//////////////////////////////////////////////////////
+			System.out.print("Please enter the amount of money tendered: ");
+			amountTendered = scan.nextDouble();
+			scan.nextLine();
+			
+// Turning both dollars and decimals into individual whole numbers
+			double change = (amountTendered - price);// initial amount of change
+			int dollarChange = (int) (change); // cast to int to give a whole number to dollars remaining
 
-		if (amountTendered < price) {
-			System.out.print("Thats not enough, please visit an ATM.");
-		} else if (amountTendered == price) {
-			System.out.print("Perfect Change!!! Thank you!!");
-		}
+			double centsPreCast = (change - dollarChange) * 100; // extracts the cents from dollars and converts the cents to whole number and remove from dollars before casting
+			double centChange = centsPreCast + .005; // adding .005 to account for remaining decimals for rounding
+			int finalCent = (int) centChange; // ensures cents is rounded to its respective true value.
+			
+//Response for Exact change or too little
 
-///////***Setting up variables for nested if statements///////////////////////////////////////////////////	
-
-		double change = (amountTendered - price);// initial amount of change
-		int dollarChange = (int) (change); // cast to int to give a whole number to dollars remaining
-		double centsPreCast = (change - dollarChange) * 100; // converting the cents to whole number and remove from
-																// dollars
-		double centChange = centsPreCast + .005; // adding .005 to account for remaining decimals for rounding
-		// before casting
-		int finalCent = (int) centChange; // ensures cents is rounded to its respective true value.
-
-		if (change > .01) {
-			System.out.print("Your total change is: ");
+			if (amountTendered < price) {
+				System.err.print("Error: amount tendered is less than price.");
+			} else if (amountTendered == price) {
+				System.out.print("Perfect Change. Thank you!");
+			}
+			
+//Extra-just cleans up the aesthetic look
+			if ((finalCent < 10 && finalCent > 1) || finalCent == 1) {
+			System.out.print("Your total change is " + dollarChange + "." + "0" + finalCent + ": ");
 			totalDollars(dollarChange);
 			totalCents(finalCent);
-		}
-
-		scan.close();
-
+			System.out.println();
+			} else if (finalCent > 10) {
+			System.out.print("Your total change is " + dollarChange + "." + finalCent + ": ");
+			totalDollars(dollarChange);
+			totalCents(finalCent);
+			System.out.println();
+			} else if (finalCent == 0){
+			System.out.print("Your total change is " + dollarChange + "." + "0" + finalCent + ": ");
+			totalDollars(dollarChange);
+			totalCents(finalCent);
+			System.out.println();
+			}
+			scan.close();
 	}// main
 
 // method for dollars
@@ -53,43 +62,40 @@ public class MakeChange {
 		int hamilton;
 		int lincoln;
 		int washington;
-//82.30
-		jackson = dollars / 20; // 4
+//twenty
+		jackson = dollars / 20;
 		if (jackson == 0) {
 			System.out.print("");
 		}
 		if (jackson > 1) {
-			System.out.print(jackson + " twenty dollar bills, "); // 3
+			System.out.print(jackson + " twenty dollar bills, ");
 		} else if (jackson == 1) {
 			System.out.print(jackson + " twenty dollar bill, ");
 		}
-
-		dollars = dollars % 20; // 2
+//ten
+		dollars = dollars % 20;
 		hamilton = dollars / 10;
 		if (hamilton == 0) {
 			System.out.print("");
 		} else {
 			System.out.print(hamilton + " ten dollar bill, ");
 		}
-		dollars = dollars % 10; // 2
+//five		
+		dollars = dollars % 10;
 		lincoln = dollars / 5;
 		if (lincoln == 0) {
 			System.out.print("");
 		} else {
 			System.out.print(lincoln + " five dollar bill, ");
 		}
-		dollars = dollars % 5; // 4
+//one		
+		dollars = dollars % 5; 
+		washington = dollars / 1;
 		if (dollars > 1) {
-			washington = dollars / 1;
-			if (washington == 0) {
-				System.out.print("");
-			}
-			if (washington > 1) {
-				System.out.print(washington + " one dollar bills, ");
+			System.out.print(washington + " one dollar bills, ");
 			} else if (washington == 1) {
 				System.out.print(washington + " one dollar bill, ");
-			}
-		} else {
+			} else {
 			System.out.println();
 		}
 
@@ -111,17 +117,17 @@ public class MakeChange {
 		} else {
 			System.out.print(quarter + " quarter, ");
 		}
-		cents = cents % 25; // 24
+		cents = cents % 25;
 //dimes
 		dime = cents / 10;
 		if (dime == 0) {
 			System.out.print("");
 		} else if (dime > 2) {
-			System.out.print(dime + " dimes, "); // 2
+			System.out.print(dime + " dimes, "); 
 		} else {
 			System.out.print(dime + " dime, ");
 		}
-		cents = cents % 10; // 4
+		cents = cents % 10; 
 //nickels
 		nickel = cents / 5;
 		if (nickel == 0) {
